4935dfc24a0f7072d2697364c2684c48597e6552
Mr Badmash <63794512+Mrbadmash@users.noreply.github.com>
2020-07-10 11:17:29 +0500

    Update Afridi.py

--- Afridi.py
+++ Afridi.py
@@ -3,11 +3,11 @@ import os.path
 from getpass import getpass
 os.system("clear")
 def banner():
-	print("\033[1;36;40m   _   _     _    _                _             ")
-	print("  /_\ | |__ | | _| |__   __ _  ___| | _____ _ __")
-	print(" //_\\| '_ \| |/ / '_ \ / _` |/ __| |/ / _ \ '__|")
-	print("/  _  \ | | |   <| | | | (_| | (__|   <  __/ |   ")
-	print("\_/ \_/_| |_|_|\_\_| |_|\__,_|\___|_|\_\___|_|   \033[0m")
+	print("\033[1;36;40mAhkhacker")
+	print("Mrbadmash")
+	print("Youtuber")
+	print("Pakistani Hacker")
+	print("I Proud Afridi\033[0m")
 	print("                \033[1;34;40m author by \033[1;33;40mIJAZ AFRIDI\033[0m")
 	print("")
 banner()
